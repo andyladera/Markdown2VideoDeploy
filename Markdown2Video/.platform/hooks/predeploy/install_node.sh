4553e9d2c6eef84290041b9f358bfc5417bd855a
nkmelndz <nkmelndz@gmail.com>
2025-06-03 17:32:16 -0500

    .shmod11

--- Markdown2Video/.platform/hooks/predeploy/install_node.sh
+++
@@ -1,9 +0,0 @@
-# #!/bin/bash
-
-# # Instala Node.js 20 LTS desde NodeSource
-# curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
-# yum install -y nodejs
-
-# # Verifica que node esté disponible
-# node -v
-# npm -v
