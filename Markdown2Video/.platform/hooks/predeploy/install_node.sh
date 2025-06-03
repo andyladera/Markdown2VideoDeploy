ce0fdb4bcf0f22a6b5aadde07aa414e7406a09f0
nkmelndz <nkmelndz@gmail.com>
2025-06-03 14:10:08 -0500

    .shmod1

--- Markdown2Video/.platform/hooks/predeploy/install_node.sh
+++ Markdown2Video/.platform/hooks/predeploy/install_node.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Instala Node.js 20 LTS desde NodeSource
-curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
-yum install -y nodejs
+# # Instala Node.js 20 LTS desde NodeSource
+# curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+# yum install -y nodejs
 
-# Verifica que node esté disponible
-node -v
-npm -v
+# # Verifica que node esté disponible
+# node -v
+# npm -v
